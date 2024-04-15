e5113c8679f4055c9d208222a7690eac53f69939
deianzen <deianzen@airpass.co.kr>
2024-04-15 13:00:51 +0900

    # Fixed Slideshow bug.

--- JavaScript/Slideshow.js
+++ JavaScript/Slideshow.js
@@ -13,16 +13,20 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("Slideshow");
   let dots = document.getElementsByClassName("dot");
+  
   if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  else if (n < 1) {slideIndex = slides.length}
+  
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
